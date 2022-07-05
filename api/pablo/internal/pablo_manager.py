cc9941bc31db6c191304c6a7e67627cbf61c84c5
Femi <ogunkolaobafemi@gmail.com>
2022-07-05 17:55:03 +0100

    .

--- api/pablo/internal/pablo_manager.py
+++ api/pablo/internal/pablo_manager.py
@@ -155,12 +155,12 @@ class PabloManager:
             try:
                 response = await self.requester.make_request(method='HEAD', url=f'https://ipfs.io/ipfs/{cid}', timeout=600)
             except ResponseException:
-                    try:
-                        response = await self.requester.make_request(method='HEAD', url=f'https://kibalabs.mypinata.cloud/ipfs/{cid}', timeout=600)
-                    except ResponseException as exception:
-                        if exception.statusCode >= 400:
-                            raise NotFoundException(message=exception.message)
-                        raise
+                try:
+                    response = await self.requester.make_request(method='HEAD', url=f'https://kibalabs.mypinata.cloud/ipfs/{cid}', timeout=600)
+                except ResponseException as exception:
+                    if exception.statusCode >= 400:
+                        raise NotFoundException(message=exception.message)
+                    raise
             headers = response.headers
         return Response(content=None, headers=headers)
 
@@ -178,11 +178,11 @@ class PabloManager:
         try:
             response = await self.requester.make_request(method='HEAD', url=f'https://ipfs.io/ipfs/{cid}', timeout=600)
         except ResponseException:
-                try:
-                    response = await self.requester.make_request(method='HEAD', url=f'https://kibalabs.mypinata.cloud/ipfs/{cid}', timeout=600)
-                except ResponseException as exception:
-                    if exception.statusCode >= 400:
-                        raise NotFoundException(message=exception.message)
-                    raise
+            try:
+                response = await self.requester.make_request(method='HEAD', url=f'https://kibalabs.mypinata.cloud/ipfs/{cid}', timeout=600)
+            except ResponseException as exception:
+                if exception.statusCode >= 400:
+                    raise NotFoundException(message=exception.message)
+                raise
         await self.s3Manager.upload_file(filePath=localFilePath, targetPath=f'{self.ipfsS3Path}/{cid}', accessControl='public-read', cacheControl=file_util.CACHE_CONTROL_FINAL_FILE, contentType=response.headers['Content-Type'])
         await file_util.remove_file(filePath=localFilePath)
