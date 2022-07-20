ff7f67d6878ebf36dc2f0807347e73ce468b80da
Krishan Patel <krishan711@gmail.com>
2022-07-20 22:23:59 +0100

    .

--- api/pablo/store/saver.py
+++ api/pablo/store/saver.py
@@ -14,7 +14,7 @@ from pablo.store.schema import UrlUploadsTable
 
 class Saver(CoreSaver):
 
-    async def create_image(self, imageId: str, format: str, filename: str, width: int, height: int, area: int, connection: Optional[DatabaseConnection] = None) -> Image:  # pylint: disable=redefined-builtin
+    async def create_image(self, imageId: str, format: str, filename: str, width: int, height: int, area: int, connection: Optional[DatabaseConnection] = None) -> Image:
         createdDate = date_util.datetime_from_now()
         updatedDate = createdDate
         values = {
@@ -66,7 +66,7 @@ class Saver(CoreSaver):
             area=area,
         )
 
-    async def create_url_upload(self, url: str, imageId: str, connection: Optional[DatabaseConnection] = None) -> UrlUpload:  # pylint: disable=redefined-builtin
+    async def create_url_upload(self, url: str, imageId: str, connection: Optional[DatabaseConnection] = None) -> UrlUpload:
         createdDate = date_util.datetime_from_now()
         updatedDate = createdDate
         values = {
